ebd4e777620e8e053a97cce1edfb8fde2c2c01f3
adityaseth8 <aseth@ucdavis.edu>
2025-03-13 20:22:03 -0700

    Initial commit. Wiki contains basic home page, categories, and AGI page from midpoint presentation

---
+++ DokuWikiStick/dokuwiki/lib/plugins/deeplautotranslate/lang/en/settings.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['api_key'] = 'DeepL-API-Key';
+$lang['api'] = 'DeepL-API that should be used';
+$lang['mode'] = 'Default operation mode of the plugin';
+$lang['show_button'] = 'Show button for forced-translations';
+$lang['push_langs'] = 'Space separated list of languages for push-translation (ISO codes)';
+$lang['glossary_ns'] = 'Namespace for the definitions of glossaries';
+$lang['blacklist_regex'] = 'Blacklist-Regex: All page names and namespaces matching this regex won\'t be translated';
+$lang['direct_regex'] = 'Direct-Regex: All page names and namespaces matching this regex will be translated in the direct mode';
+$lang['editor_regex'] = 'Editor-Regex: All page names and namespaces matching this regex will be translated in the editor mode';
+$lang['ignored_expressions'] = 'Expressions that won\'t be translated, seperated by \':\'';
+$lang['default_lang_in_ns'] = 'The default language is in a namespace (should normally not be the case)';
+$lang['keep_relative'] = 'Do not rewrite relative links when translating';
---
+++ DokuWikiStick/dokuwiki/lib/plugins/deeplautotranslate/readme.md
@@ -0,0 +1,5 @@
+# DokuWiki DeepL Autotranslate Plugin
+This plugin automatically creates translations based on the [Translation plugin](https://www.dokuwiki.org/plugin:translation) page structure using the [DeepL](https://deepl.com) API. 
+
+## Usage and configuration
+For usage- and configuration instructions please visit the [Dokuwiki-Plugin-Page](https://www.dokuwiki.org/plugin:deeplautotranslate) of this Plugin.
